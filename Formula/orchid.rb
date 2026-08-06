ad471a03cd279dbb38766f8db3c7aa716fff64a2
Bilal <bahma060@gmail.com>
2026-08-06 07:46:41 -0400

    T007: re-pin Formula/orchid.rb checksum for this candidate

--- Formula/orchid.rb
+++ Formula/orchid.rb
@@ -7,7 +7,7 @@ class Orchid < Formula
   desc "Deterministic multi-agent orchestrator for AI coding CLIs"
   homepage "https://github.com/bilal-/orchid"
   url "https://github.com/bilal-/orchid/releases/download/v1.0.0/orchid-1.0.0.tar.gz"
-  sha256 "6aa40d457908aad2c936dc353cf3cbe333e6b715ab05e1bad47b338f9bda80a7"
+  sha256 "250fd302b0a64956bbb3132b72d9217ec8d4c0b163c52c0d13c167f7183805c6"
   license "MIT"
   version "1.0.0"
 
